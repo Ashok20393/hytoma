0d9b4e5a02369bef26600e70784e1973dc6b6e7d
Ashok20393 <ashokcivil27@gmail.com>
2026-04-30 16:57:31 +0530

    chnages done

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173","https://hytoma.vercel.app"],
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS" , "PATCH"],
     allow_headers=["*"],
 )
 
